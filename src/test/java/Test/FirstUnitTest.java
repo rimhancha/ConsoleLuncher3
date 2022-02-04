74642108769d4fa6ca981446b9a7523ad75c12cd
rim hancha <rimhancha1985gmail.com>
2022-02-04 22:57:58 +0100

    add p

---
+++ src/test/java/Test/FirstUnitTest.java
@@ -0,0 +1,14 @@
+package Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+public class FirstUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
